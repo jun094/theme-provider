235a883708db90d9db17e458a7679eea631f4111
jun094 <jun094@naver.com>
2024-09-16 21:26:35 +0900

    feat: tsup 번들 옵션 수정

--- tsup.config.js
+++ tsup.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
+	format: ["esm"],
+	legacyOutput: true, // output 결과물이 '.mjs'로 나오지 않게 함
 	splitting: false,
 	sourcemap: true,
 	clean: true,
+	dts: true,
 });
